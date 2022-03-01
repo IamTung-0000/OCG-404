57dd578c55308f7e17a700ace536fe0c469186e3
IamTung-0000 <iamtung.asia@gmail.com>
2022-03-01 18:35:36 +0700

    update shader

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let camera, scene, renderer;
+let clock;
 let last_cameraPositionX;
-let frontLight, leftLight, rightLight, topLight,ambientLight;
-const defaultCameraPosition = new THREE.Vector3( 0.9413746101665292, 1.8353249151318962, 2.1510883355862758 );
+let frontLight, leftLight, rightLight, topLight,ambientLight, light;
+const defaultCameraPosition = new THREE.Vector3( 0.766, 1.392, 1.603 );
 const mouse = new THREE.Vector2();
 const target = new THREE.Vector2();
 const windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight / 2 );
@@ -9,6 +10,7 @@ const windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight 
 renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
 camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 20 );
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
+let model;
 
 
 init();
@@ -18,40 +20,28 @@ render();
 
 function Light() {
 
-    frontLight = new THREE.SpotLight( 0xffffff );
-    frontLight.position.set( 0, 0, -600 );
-    scene.add( frontLight );
+    light = new THREE.PointLight(0xffffff);
+    // We want it to be very close to our character
+    light.position.set(-5,-2,3);
+    light.intensity = 0.1;
+    scene.add(light);
 
-    leftLight = new THREE.SpotLight( 0xffffff );
-    leftLight.position.set( -600, 0, 0 );
-    scene.add( leftLight );
+    const light_helper = new THREE.PointLightHelper( light, 1 );
+    scene.add( light_helper );
 
-    rightLight = new THREE.SpotLight( 0xffffff );
-    rightLight.position.set( 600, 0, 0 );
-    scene.add( rightLight );
-
-    topLight = new THREE.SpotLight( 0xffffff );
+    topLight = new THREE.SpotLight( 0x005BFF );
     topLight.position.set( 0, 600, 200 );
-    topLight.intensity = 3;
+    topLight.intensity = 10;
     scene.add( topLight );
-
-
-    const front_helper = new THREE.SpotLightHelper( frontLight, 20 );
-    scene.add( front_helper );
-
-    // const left_helper = new THREE.SpotLightHelper( leftLight, 20 );
-    // scene.add( left_helper );
-
-
-    // const right_helper = new THREE.SpotLightHelper( rightLight, 20 );
-    // scene.add( right_helper );
 
     // const top_helper = new THREE.SpotLightHelper( topLight, 20 );
     // scene.add( top_helper );
 
 
-    ambientLight = new THREE.AmbientLight( 0x222222 );
-    scene.add( ambientLight );
+
+    // ambientLight = new THREE.AmbientLight( 0x005BFF );
+    // ambientLight.intensity = 5;
+    // scene.add( ambientLight );
 
 }
 
@@ -61,9 +51,7 @@ function onMouseMove( event ) {
 	mouse.y = ( event.clientY - windowHalf.y );
     // console.log(mouse.x);
     // console.log(mouse.y);
-
 }
-
 
 
 function init() {
@@ -76,11 +64,124 @@ function init() {
 
     const loader = new THREE.GLTFLoader().setPath( 'Models/' );
 						loader.load( '404.gltf', function ( gltf ) {
-							scene.add( gltf.scene );
-                            console.log(gltf);
-                            gltf.scene.position.y = -1;
+                            model = gltf;
+							scene.add( model.scene );
+                            model.scene.position.y = -1;   
+                            // console.log(model.scene);   
+
+                            var uniforms = THREE.UniformsUtils.merge(
+                                [THREE.UniformsLib['lights'],
+                                {
+                                    lightIntensity: {type: 'f', value: 1.0},
+                                }
+                                ]
+                            )
+                            console.log(uniforms)
+
+                            var material = new THREE.ShaderMaterial({
+                                uniforms: THREE.UniformsUtils.merge([
+                                    THREE.UniformsLib['lights'],
+                                    {
+                                        lightIntensity: {type: 'f', value: 0.6},
+                                        textureSampler: {type: 't', value: null},
+                                        color1: {
+                                            value: new THREE.Color(0x005BFF) //top
+                                          },
+                                          color2: {
+                                            value: new THREE.Color(0x00000) //bottom
+                                          }
+                                    }
+                                ]),
+
+                                vertexShader: `
+                                varying vec2 vUv;
+                                varying vec3 vecPos;
+                                varying vec3 vecNormal;
+                                 
+                                void main() {
+                                  vUv = uv;
+                                  // Since the light is in camera coordinates,
+                                  // I'll need the vertex position in camera coords too
+                                  vecPos = (modelViewMatrix * vec4(position, 1.0)).xyz;
+                                  // That's NOT exacly how you should transform your
+                                  // normals but this will work fine, since my model
+                                  // matrix is pretty basic
+                                  vecNormal = (modelViewMatrix * vec4(normal, 0.0)).xyz;
+                                  gl_Position = projectionMatrix *
+                                                vec4(vecPos, 1.0);
+                                }
+                                `,
+                                fragmentShader: `
+
+                                uniform vec3 color1;
+                                uniform vec3 color2;
+
+                                
+                                precision highp float;
+ 
+                                varying vec2 vUv;
+                                varying vec3 vecPos;
+                                varying vec3 vecNormal;
+                                 
+                                uniform float lightIntensity;
+                                uniform sampler2D textureSampler;
+                                 
+                                struct PointLight {
+                                    vec3 color;
+                                    vec3 position; // light position, in camera coordinates
+                                    float distance; // used for attenuation purposes. Since
+                                                    // we're writing our own shader, it can
+                                                    // really be anything we want (as long as
+                                                    // we assign it to our light in its
+                                                    // "distance" field
+                                  };
+                                   
+                                  uniform PointLight pointLights[NUM_POINT_LIGHTS];
+                                   
+                                  void main(void) {
+                                    // Pretty basic lambertian lighting...
+                                    vec4 addedLights = vec4(0.0,
+                                                            0.0,
+                                                            0.0,
+                                                            1.0);
+                                    for(int l = 0; l < NUM_POINT_LIGHTS; l++) {
+                                        vec3 lightDirection = normalize(vecPos
+                                                              - pointLights[l].position);
+                                        addedLights.rgb += clamp(dot(-lightDirection,
+                                                                 vecNormal), 0.0, 1.0)
+                                                           * pointLights[l].color
+                                                           * lightIntensity;
+                                    }
+                                    // gl_FragColor = addedLights;
+                                    vec4 gradient = vec4(mix(color1, color2, vUv.y), 1.0);
+                                    gl_FragColor = gradient + (addedLights - 0.2);
+                                  }
+                                `,
+                                lights: true
+
+                              });      
+                              
+                              
+                            model.scene.traverse( function(node) {
+                                
+
+                                if (node.isMesh){
+                                    if (node.material.name == "side") {
+                                        console.log(node.material.name);
+                                        node.material = material;
+                                    }
+                                    
+                                    // node.material = material;
+                                    // node.blend = 0.4;
+                                    // node.opacity = 0.5;
+                                    // node.transparent = true;
+                                    // console.log(node.opacity);
+                                }
+                            });
+
 							render();
 						} );
+                                         
 
     // renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -89,11 +190,11 @@ function init() {
 
     // const controls = new THREE.OrbitControls( camera, renderer.domElement );
     // controls.addEventListener( 'change', render ); // use if there is no animation loop
+    controls.enabled = false;
     controls.enableDamping = true;
-    controls.enableZoom = false;
-    controls.autoRotate = false;
-    controls.minDistance = 2;
-    controls.maxDistance = 10;
+    // controls.enableZoom = true;
+    // controls.minDistance = 2;
+    // controls.maxDistance = 10;
     controls.target.set( 0, 0, - 0.2 );
 
     controls.update();
@@ -101,6 +202,7 @@ function init() {
     document.addEventListener( 'mousemove', onMouseMove, false );
     window.addEventListener( 'resize', onWindowResize );
 
+    clock = new THREE.Clock();
 
     Light();
 
@@ -117,10 +219,22 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 	renderer.setSize( width, height );
     render();
+
 }
 
 
 function animate() {
+
+    // console.log(clock.getElapsedTime());
+    if (model!= null ) {
+        // console.log(model.scene.children[0]);
+        model.scene.children[0].position.y = Math.sin(clock.getElapsedTime()*4)*0.05;
+        model.scene.children[2].position.y = Math.sin((clock.getElapsedTime()*4)-1)*0.05;
+        model.scene.children[1].position.y = Math.sin((clock.getElapsedTime()*4)-2)*0.05;
+        
+    }
+
+
 
     target.x = ( 1 - mouse.x ) * 0.001;
     target.y = ( 1 - mouse.y ) * 0.001;
@@ -143,18 +257,21 @@ function animate() {
     controls.update();
 
     requestAnimationFrame( animate );
+    
 
     render();
+    
 
 }
 
 
 function render() {
 
-    if (camera.position.x != last_cameraPositionX) {
-        console.log(camera.position);
-        last_cameraPositionX = camera.position.x;
-    }
+    
+    // if (camera.position.x != last_cameraPositionX) {
+    //     console.log(camera.position);
+    //     last_cameraPositionX = camera.position.x;
+    // }
 
     renderer.render( scene, camera );
 
